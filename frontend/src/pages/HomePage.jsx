d681df7b83d4ef95b7f26df43ce619cc6d845405
THOMAS LOFFICIAL <thomaslofficial@gmail.com>
2022-10-28 11:29:05 +0200

    structures homepage / filterscomponent faites

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import "../styles/HomePage.css";
+import FiltersComponent from "../components/filters-comp/FiltersComponent";
 
-export default function HomePage() {
-  return <div>HomePage</div>;
+function HomePage() {
+  return (
+    <div>
+      <section className="header">header</section>
+      <div className="main">
+        <h2>Oh my brew!</h2>
+        <h3>Let the dogs out!</h3>
+        <FiltersComponent />
+        <img src="src/assets/oh-my-brew-icon-dog-ok.png" alt="dog" />
+      </div>
+      <div className="beerDisplay">
+        <div className="beerCardList">BeerCardList</div>
+        <div id="illustration">
+          <img src="src/assets/oh-my-brew-icon-dog-not-ok.png" alt="dog" />
+        </div>
+      </div>
+      <section className="footer">footer</section>
+    </div>
+  );
 }
+
+export default HomePage;
