6b3dbabccea36e0153c13463bcf107140bca0f05
Olivier Tremelo <oliviertremelo@gmail.com>
2022-10-28 11:56:26 +0200

    création cards details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import HomePage from "./pages/HomePage";
 
 import "./App.css";
+import BeerCard from "./components/beer-card-elmt/BeerCard";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
       <p>coucou</p>
+
+      <BeerCard />
     </div>
   );
 }
