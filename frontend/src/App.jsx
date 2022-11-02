fdb95d1673ef784bb25e7fe4246f29337aa59576
NaoSan9 <naomi.santos@outlook.fr>
2022-11-02 11:13:20 +0100

    Import modified in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import HomePage from "./pages/HomePage";
 
-import "./App.css";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <HomePage />
-      <p>coucou</p>
     </div>
   );
 }
