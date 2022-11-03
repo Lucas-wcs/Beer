6b446455a7006d26b699611e0366151b340b7239
NaoSan9 <naomi.santos@outlook.fr>
2022-11-03 09:14:18 +0100

    Functional Header in desktop

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import "./styles/App.css";
 import AgeGate from "./pages/AgeGate";
-import HomePage from "./pages/HomePage";
+
 
 function App() {
   return (
     <div className="App">
       <AgeGate />
-      <HomePage />
     </div>
   );
 }
