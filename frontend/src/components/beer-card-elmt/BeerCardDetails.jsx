e5b007cf5df0f7677b53b4bed6341eb237bb42f0
THOMAS LOFFICIAL <thomaslofficial@gmail.com>
2022-11-09 09:47:31 +0100

    mise à jour avec branche dev

--- frontend/src/components/beer-card-elmt/BeerCardDetails.jsx
+++ frontend/src/components/beer-card-elmt/BeerCardDetails.jsx
@@ -1,58 +1,123 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "../../styles/beer-card-elmt/BeerCardStyle.css";
 
 import PropTypes from "prop-types";
 
 function BeerCardDetails({ close }) {
+  const [back, setBack] = useState(false);
+
+  const handleBack = () => {
+    setBack(!back);
+  };
+
   return (
-    <div className="beerCardDetails">
-      <div className="marg">
-        <h2 className="title">India Session Lager - Prototype Challenge</h2>
-        <div className="imgAndText">
-          <div className="imgAndTagLine">
-            <img
-              className="img"
-              src="https://static.unepetitemousse.fr/produits/bieres/brewdog/brewdog-PunkIPA-2021_1800w.jpg"
-              alt=""
-            />
-            <h4>Refreshing Hop Fix</h4>
-          </div>
-          <div className="blockText">
-            <div className="dateAndAlc">
-              <h5 className="date">First brewed: 11/1/2015</h5>
-              <h5 className="alc">Alc: 4,4%</h5>
+    <div className="flipcard">
+      {!back ? (
+        <div className="beerCardDetailsFront">
+          <div className="marg">
+            <h3 className="title">India Session Lager - Prototype Challenge</h3>
+            <div className="imgAndText">
+              <div className="imgAndTagLine">
+                <img
+                  className="img"
+                  src="https://static.unepetitemousse.fr/produits/bieres/brewdog/brewdog-PunkIPA-2021_1800w.jpg"
+                  alt=""
+                />
+                <h4 className="taglineBeer">Refreshing Hop Fix</h4>
+              </div>
+              <div className="blockText">
+                <div className="dateAndAlc">
+                  <p className="date">First brewed: 11/1/2015</p>
+                  <p className="alc">Alc: 4,4%</p>
+                </div>
+                <div className="brewDetails">
+                  <p>
+                    <span>Bitterness:</span> o o o o o IBU 23 (International
+                    Bitterness Unit)
+                  </p>
+                  <p>
+                    <span>Color:</span>
+                    <div className="brewColor" /> EBC 20 (European Brewery
+                    Convention)
+                  </p>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Necessitatibus rem iste repudiandae atque dolorem deserunt
+                  itaque vero. Quia quis illum beatae dolore distinctio mollitia
+                  eos dolor laboriosam tempora!
+                </p>
+                <button className="btn" type="button">
+                  Add to Favorite{" "}
+                </button>
+              </div>
             </div>
-            <div className="brewDetails">
-              <h5>
-                Bitterness: o o o o o IBU 23 (International Bitterness Unit)
-              </h5>
-              <h5>
-                Color: <div className="brewColor" /> EBC 20 (European Brewery
-                Convention)
-              </h5>
+            <div className="buttonsFlecheCroix">
+              <button onClick={close} className="btnCroix" type="button">
+                X
+              </button>
+              <button type="button" onClick={handleBack}>
+                --{">"}
+              </button>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Necessitatibus rem iste repudiandae atque dolorem deserunt itaque
-              vero. Quia quis illum beatae dolore distinctio mollitia eos dolor
-              laboriosam tempora!
-            </p>
-            <button className="btn" type="button">
-              Add to Favorite{" "}
-            </button>
           </div>
         </div>
-        <div className="buttonsFlecheCroix">
-          <button onClick={close} className="btnCroix" type="button">
-            X
-          </button>
-          <button className="btnFleche" type="button">
-            --{">"}
-          </button>
+      ) : (
+        <div>
+          <div className="card-is-flipped">
+            <div className="marg">
+              <h3 className="title">
+                India Session Lager - Prototype Challenge
+              </h3>
+              <div className="imgAndText">
+                <div className="imgAndTagLine">
+                  <img
+                    className="img"
+                    src="https://static.unepetitemousse.fr/produits/bieres/brewdog/brewdog-PunkIPA-2021_1800w.jpg"
+                    alt=""
+                  />
+                  <h4 className="taglineBeer">Refreshing Hop Fix</h4>
+                </div>
+                <div className="blockText">
+                  <div className="dateAndAlc">
+                    <p className="date">First brewed: 11/1/2015</p>
+                    <p className="alc">Alc: 4,4%</p>
+                  </div>
+                  <div className="brewDetails">
+                    <p>
+                      <span>Bitterness:</span> o o o o o IBU 23 (International
+                      Bitterness Unit)
+                    </p>
+                    <p>
+                      <span>Color:</span>
+                      <div className="brewColor" /> EBC 20 (European Brewery
+                      Convention)
+                    </p>
+                  </div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Necessitatibus rem iste repudiandae atque dolorem deserunt
+                    itaque vero. Quia quis illum beatae dolore distinctio
+                    mollitia eos dolor laboriosam tempora!
+                  </p>
+                  <button className="btn" type="button">
+                    Add to Favorite{" "}
+                  </button>
+                </div>
+              </div>
+              <div className="buttonsFlecheCroix">
+                <button onClick={close} className="btnCroix" type="button">
+                  X
+                </button>
+                <button type="button" onClick={handleBack}>
+                  --{">"}
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </div> // flipcard
   );
 }
 
