8827003b6e1a26f415c9612a841e22a216068069
NaoSan9 <naomi.santos@outlook.fr>
2022-11-09 12:29:26 +0100

    Footer functional and modifications on header

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,8 +3,8 @@ import "../styles/HomePage.css";
 
 import Header from "@components/Header";
 import BeerCard from "@components/beer-card-elmt/BeerCard";
-import FiltersComponent from "../components/filters-comp/FiltersComponent";
 import Footer from "@components/Footer";
+import FiltersComponent from "../components/filters-comp/FiltersComponent";
 
 function HomePage() {
   return (
